f140e076e08da853be2845195efb388c70e44da7
Michael Nguyen <mnguyen2000@gmail.com>
2023-07-28 02:40:11 -0700

    Added new MovieList, application can now be sorted by genres and categories, added icons

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  console.log('Movies');
+  const [page, setPage] = useState(1);
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
+  const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page });
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occurred.';
   return (
     <div>
-      Movies
+      <MovieList movies={data} />
     </div>
   );
 };
---
+++ src/services/TMDB.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+// https://api.themoviedb.org/3/movie/popular
+
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    //* Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+    //* Get Movies by [type]
+    getMovies: builder.query({
+      query: ({ genreIdOrCategoryName, page }) => {
+        //* Get Movies by Category
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+        }
+        //* Get Movies by Genre
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+        //* Get Popular Movies
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetGenresQuery,
+  useGetMoviesQuery,
+} = tmdbApi;
