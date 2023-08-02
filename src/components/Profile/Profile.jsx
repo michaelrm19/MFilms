06a6cc5d3cb4a6ee6e04c2b10e0d7b9e2862ffd2
Michael Nguyen <mnguyen2000@gmail.com>
2023-08-01 17:47:24 -0700

    Added Movie Information, Styling for Movie Information, Actors, Overview, and Buttons

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
+
+import { userSelector } from '../../features/auth';
+
+// Get access to profile name or ID from redux state
+// Display in the profile component
 
 const Profile = () => {
-  console.log('Profile');
+  const { user } = useSelector(userSelector);
+  const favoriteMovies = [];
+  const logout = () => {
+    // To log out a user, it is very simple. Just clear the local storage
+    localStorage.clear();
+    // reload the page
+    window.location.href = '/';
+  };
   return (
-    <div>
-      Profile
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom> My Profile </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? <Typography variant="h5"> Add favorites or watchlist some movies to see them here!</Typography>
+        : (
+          <Box>
+            FAVORITE MOVIES
+          </Box>
+        )}
+    </Box>
   );
 };
 
