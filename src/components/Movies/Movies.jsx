3bb2cff22a518185dd8364e042551d15310f8174
Michael Nguyen <mnguyen2000@gmail.com>
2023-08-02 01:45:49 -0700

    Bug Fixing, and Responsiveness Adjustments

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -10,6 +10,8 @@ const Movies = () => {
   const [page, setPage] = useState(1);
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
+  const numberOfMovies = lg ? 16 : 18;
 
   if (isFetching) {
     return (
@@ -34,7 +36,7 @@ const Movies = () => {
   if (error) return 'An error has occurred.';
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
   );
